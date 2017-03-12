7dedb1402e116781d39d3dfe3815ec11fa6a7741
Iván Rodríguez <ivan.traceur@gmail.com>
2017-03-12 00:08:21 +0000

    Añadimos el tutorial 13
    Añadimos el tutorial sobre la programación de un motor DC

---
+++ Tutorial_13/Tutoria_13.ino
@@ -0,0 +1,21 @@
+/* Copyright (C) 2017 Iván Rodríguez Méndez
+This software is distributed under the GNU General Public
+Licence (version 3 or later); please refer to the file
+Licence.txt, included with the software, for details.
+*/
+
+//Tutorial 13: Control simple de motor DC
+
+int potPin = 0; // Pin en el que hacemos la conexion del potenciometro
+int transistorPin = 9; // Pin para enviar la PWM por el transistor
+int Valorpot = 0; // Valor que nos devuelve el potenciometro
+
+void setup() {
+ pinMode(transistorPin, OUTPUT); //Definimos como pin de salida el transistor
+}
+void loop() {
+ // Leemos el valor del potenciometro y lo normaliamos entre 0 y 255
+ Valorpot = analogRead(potPin) / 4;
+ // Usamos el mismo valor que hemos leido para mandar al motor DC
+ analogWrite(transistorPin, Valorpot);
+}
