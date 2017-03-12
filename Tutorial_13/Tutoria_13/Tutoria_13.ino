b717aebfbbd9b69e58f4dfc1e2c5e4d3708c83d7
tidus747 <ivan.traceur@gmail.com>
2017-03-12 13:11:12 +0000

    Mejoras en el tutorial 13

--- Tutorial_13/Tutoria_13/Tutoria_13.ino
+++ Tutorial_13/Tutoria_13/Tutoria_13.ino
@@ -6,16 +6,19 @@ Licence.txt, included with the software, for details.
 
 //Tutorial 13: Control simple de motor DC
 
-int potPin = 0; // Pin en el que hacemos la conexion del potenciometro
+int potPin = 1; // Pin en el que hacemos la conexion del potenciometro
 int transistorPin = 9; // Pin para enviar la PWM por el transistor
 int Valorpot = 0; // Valor que nos devuelve el potenciometro
 
 void setup() {
+  Serial.begin(9600);
  pinMode(transistorPin, OUTPUT); //Definimos como pin de salida el transistor
 }
 void loop() {
  // Leemos el valor del potenciometro y lo normaliamos entre 0 y 255
  Valorpot = analogRead(potPin) / 4;
+ Serial.print(Valorpot);
  // Usamos el mismo valor que hemos leido para mandar al motor DC
  analogWrite(transistorPin, Valorpot);
+ delay(500);
 }
